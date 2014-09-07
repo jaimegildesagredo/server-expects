16c258778f01bbbf7e7add1ae36a48adf8f5838e
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2014-09-07 20:24:36 +0200

    Ubuntu 14.04 support

--- Makefile
+++ Makefile
@@ -5,19 +5,32 @@ TEST_DIST = data/$(NAME).tar.gz
 
 test: sdist
 	cp $(DIST) $(TEST_DIST)
-	kitchen test
+	kitchen test $(KITCHEN_INSTANCE)
 
 sdist:
 	python setup.py sdist
 
 list:
-	kitchen list
+	kitchen list $(KITCHEN_INSTANCE)
 
 destroy:
-	kitchen destroy
+	kitchen destroy $(KITCHEN_INSTANCE)
 
 distclean: clean
 	rm -f $(DIST)
 
 clean:
 	rm -f $(TEST_DIST)
+
+ec2_test: sdist
+	ssh-keygen -q -b 2048 -t rsa -f $(AWS_SSH_KEY_PATH) -N ''
+	aws ec2 import-key-pair --key-name $(AWS_SSH_KEY_ID) --public-key-material "`cat $(AWS_SSH_KEY_PATH).pub`"
+	cp $(DIST) $(TEST_DIST)
+	KITCHEN_YAML=.kitchen.cloud.yml kitchen test $(KITCHEN_INSTANCE)
+
+ec2_list:
+	KITCHEN_YAML=.kitchen.cloud.yml kitchen list $(KITCHEN_INSTANCE)
+
+ec2_destroy:
+	KITCHEN_YAML=.kitchen.cloud.yml kitchen destroy $(KITCHEN_INSTANCE)
+	aws ec2 delete-key-pair --key-name $(AWS_SSH_KEY_ID)
---
+++ data/ubuntu-12.04-bootstrap.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get update
+apt-get remove -y ruby1.8
+apt-get install -y python python-virtualenv git ruby1.9.1-full
+
+pip install `dirname $0`/server-expects.tar.gz
+
+cat `dirname $0`/ubuntu-12.04-environment.sh > /etc/bash.bashrc
+. /etc/bash.bashrc
+
+apt-get install -y $TEST_AN_INSTALLED_DEB_NAME=$TEST_AN_INSTALLED_DEB_VERSION
+apt-get remove -y $TEST_AN_UNINSTALLED_DEB
+
+pip install $TEST_AN_INSTALLED_EGG_NAME==$TEST_AN_INSTALLED_EGG_VERSION
+pip install -e $TEST_AN_INSTALLED_EDITABLE_EGG_URL
+pip uninstall -y $TEST_AN_UNINSTALLED_EGG || echo 'Already uninstalled'
+
+virtualenv $TEST_A_VIRTUALENV_PATH
+$TEST_A_VIRTUALENV_PATH/bin/pip install $TEST_A_VIRTUALENV_INSTALLED_EGG
+$TEST_A_VIRTUALENV_PATH/bin/pip uninstall -y $TEST_A_VIRTUALENV_UNINSTALLED_EGG || echo 'Already uninstalled'
+
+rm -rf $TEST_A_NONEXISTENT_VIRTUALENV_PATH
+
+echo "$TEST_AN_UNREACHABLE_IP $TEST_AN_UNREACHABLE_HOST" >> /etc/hosts
+
+# MySQL
+apt-get install -y mysql-server
+
+if ! mysql -u"$TEST_A_MYSQL_EXISTENT_USER" -p"$TEST_A_MYSQL_VALID_PASSWORD" -e 'SELECT 1'
+then
+    mysql -e "CREATE USER '$TEST_A_MYSQL_EXISTENT_USER' IDENTIFIED BY '$TEST_A_MYSQL_VALID_PASSWORD'"
+fi
+
+mysql -e "CREATE DATABASE IF NOT EXISTS $TEST_A_MYSQL_EXISTENT_DATABASE"
+mysql -e "GRANT SELECT ON $TEST_A_MYSQL_EXISTENT_DATABASE.* TO '$TEST_A_MYSQL_EXISTENT_USER' IDENTIFIED BY '$TEST_A_MYSQL_VALID_PASSWORD'"
+mysql -e "DROP DATABASE IF EXISTS $TEST_A_MYSQL_NONEXISTENT_DATABASE"
